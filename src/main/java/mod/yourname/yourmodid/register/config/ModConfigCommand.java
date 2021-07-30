1b7492e779a59e849b2f3ae8aa534876e4c8a973
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-07-30 22:01:18 +0300

    use config command instead of gui

--- src/main/java/mod/yourname/yourmodid/register/config/ModConfigCommand.java
+++ src/main/java/mod/yourname/yourmodid/register/config/ModConfigCommand.java
@@ -1,5 +1,6 @@
 package mod.yourname.yourmodid.register.config;
 
+import com.google.common.eventbus.Subscribe;
 import com.simibubi.create.foundation.config.ui.BaseConfigScreen;
 import com.simibubi.create.foundation.gui.AbstractSimiScreen;
 import com.simibubi.create.foundation.gui.ScreenOpener;
@@ -10,9 +11,11 @@ import mod.yourname.yourmodid.CreateAddon;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.Widget;
+import net.minecraft.command.Commands;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
@@ -22,16 +25,7 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 @Mod.EventBusSubscriber(Dist.CLIENT)
-public class ConfigOptionRenderer {
-    /**
-     * The offset to render the button, for addon compatibility.
-     * <p>
-     * You should probably +add to this instead of =setting it directly, again, for compatibility.
-     */
-    public static int renderOffset = 60;
-
-    public static Field widgetListField = ObfuscationReflectionHelper.findField(AbstractSimiScreen.class, "widgets");
-
+public class ModConfigCommand {
     public static class ModConfigScreen extends BaseConfigScreen {
         public ModConfigScreen(Screen parent, @Nonnull String modID) {
             super(parent, modID);
@@ -43,20 +37,13 @@ public class ConfigOptionRenderer {
                 .withTitles("Client Settings", "World Generation Settings", "Gameplay Settings")
                 .withSpecs(null, null, ModConfigs.SERVER.specification);
     }
+    public static final String configCommandName = BuildConfig.MODID;
 
     @SubscribeEvent
-    public static void render(GuiScreenEvent.InitGuiEvent.Post event) throws IllegalAccessException {
-        Screen screen = event.getGui();
-        if (screen.getClass().equals(BaseConfigScreen.class)) {
-            BaseConfigScreen configScreen = (BaseConfigScreen) screen;
-            TextStencilElement buttonText = new TextStencilElement(Minecraft.getInstance().font, new StringTextComponent(BuildConfig.DISPLAY_NAME)).centered(true, true);
-            BoxWidget buttonWidget;
-            buttonWidget = new BoxWidget(configScreen.width / 2 - 100, configScreen.height / 2 + renderOffset, 200, 16).showingElement(buttonText);
-
-            buttonWidget.withCallback(() -> ScreenOpener.open(createScreen(configScreen)));
-            buttonText.withElementRenderer(BoxWidget.gradientFactory.apply(buttonWidget));
-            List<Widget> widgets = (List<Widget>) widgetListField.get(configScreen);
-            widgets.add(buttonWidget);
-        }
+    public static void commands(RegisterCommandsEvent event) {
+        event.getDispatcher().register(Commands.literal(configCommandName).then(Commands.literal("config").executes((ctx) -> {
+            ScreenOpener.open(createScreen(null));
+            return 1;
+        })));
     }
 }
