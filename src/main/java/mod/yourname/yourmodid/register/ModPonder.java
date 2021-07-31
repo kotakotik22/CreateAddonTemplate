2421f97174b81ef5d519d360d99b85bd4d461ecf
kotakotik22 <61428759+kotakotik22@users.noreply.github.com>
2021-07-31 22:03:14 +0300

    update to 0.3.2c

--- src/main/java/mod/yourname/yourmodid/register/ModPonder.java
+++ src/main/java/mod/yourname/yourmodid/register/ModPonder.java
@@ -1,23 +1,20 @@
 package mod.yourname.yourmodid.register;
 
 import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.foundation.ponder.PonderLocalization;
 import com.simibubi.create.foundation.ponder.PonderRegistry;
 import mod.yourname.yourmodid.BuildConfig;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
 
 public class ModPonder {
     public static void register() {
-        // Make sure to include your modid in your ponder id! Otherwise it won't generate the lang!
+        PonderRegistry.startRegistration(BuildConfig.MODID);
+        // Put your ponder here!
+        PonderRegistry.endRegistration();
     }
 
     public static void generateLang(CreateRegistrate registrate, GatherDataEvent event) {
         register();
-        PonderRegistry.provideLangEntries().getAsJsonObject().entrySet().forEach(e -> {
-            String k = e.getKey();
-            String v = e.getValue().getAsString();
-            if (k.contains(BuildConfig.MODID + ".")) {
-                registrate.addRawLang(k, v);
-            }
-        });
+        PonderLocalization.provideRegistrateLang(registrate);
     }
 }
--- src/main/java/mod/yourname/yourmodid/register/config/ModServerConfig.java
+++ src/main/java/mod/yourname/yourmodid/register/config/ModServerConfig.java
@@ -1,4 +1,56 @@
 package mod.yourname.yourmodid.register.config;
 
+import com.simibubi.create.foundation.block.BlockStressDefaults;
+import com.simibubi.create.foundation.block.BlockStressValues;
+import mod.yourname.yourmodid.BuildConfig;
+import net.minecraft.block.Block;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.ForgeConfigSpec;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModServerConfig extends ModConfigs.Config {
+    public static class StressValues extends ModConfigs.Config implements BlockStressValues.IStressValueProvider {
+        protected Map<ResourceLocation, ConfigFloat> impacts = new HashMap<>();
+        protected Map<ResourceLocation, ConfigFloat> capacities = new HashMap<>();
+
+                {
+            BlockStressDefaults.DEFAULT_IMPACTS.forEach((r, i) -> {
+                if (r.getNamespace().equals(BuildConfig.MODID)) {
+                    impacts.put(r, f(i.floatValue(), Float.MAX_VALUE, r.getPath()));
+                }
+            });
+            BlockStressDefaults.DEFAULT_CAPACITIES.forEach((r, i) -> {
+                if (r.getNamespace().equals(BuildConfig.MODID)) {
+                    capacities.put(r, f(i.floatValue(), Float.MAX_VALUE, r.getPath()));
+                }
+            });
+            BlockStressValues.registerProvider(BuildConfig.MODID, this);
+        }
+
+        @Override
+        public double getImpact(Block block) {
+            ConfigFloat c = impacts.get(block.getRegistryName());
+            return c == null ? 0d : c.get();
+        }
+
+        @Override
+        public double getCapacity(Block block) {
+            ConfigFloat c = capacities.get(block.getRegistryName());
+            return c == null ? 0d : c.get();
+        }
+
+        @Override
+        public boolean hasImpact(Block block) {
+            return impacts.containsKey(block.getRegistryName());
+        }
+
+        @Override
+        public boolean hasCapacity(Block block) {
+            return capacities.containsKey(block.getRegistryName());
+        }
+    }
+
+//    public StressValues stressValues = nested(0, StressValues::new);
 }
